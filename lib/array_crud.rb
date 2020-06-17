9a6f5748952e677341d4e913b1fab16783d57abc
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-17 13:49:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Eminem", "Logic", "Kendrick Lamar", ]
+  ["Eminem", "Logic", "Kendrick Lamar", "Wu-tang Clan"]
 end
 
 def add_element_to_end_of_array(array, element)
